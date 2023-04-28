908bdd3edf606ef222160776c16e94e433258196
Alexey <alexeistarikow@yandex.ru>
2023-04-28 19:21:42 +0300

    add Lesson3

---
+++ java_projects/program.java
@@ -0,0 +1,14 @@
+package java_projects;
+
+
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+       int a = 125;
+       a= ++a - a--;
+       System.out.println(a);
+    }
+}
